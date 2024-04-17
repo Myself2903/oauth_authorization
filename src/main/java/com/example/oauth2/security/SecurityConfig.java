e77df45a25539eddbd1476b982899603f06b6e3a
Myself2903 <hassanbarranco2003123@gmail.com>
2024-04-16 20:12:57 -0500

    Definicion de una página simple de autenticación con oauth2. Disponible con Github, la de Google tiene bloqueada la app debido a la falta de dominio (Se ejecuta en localhost). Tampoco funciona la opcion de logout.

---
+++ src/main/java/com/example/oauth2/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.oauth2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers("/","/index.html","/oauth_login.html").permitAll()
+            .anyRequest().authenticated()
+        )
+        .oauth2Login(l -> l
+            .loginPage("/oauth_login.html")
+            .defaultSuccessUrl("/")
+        )
+        .logout(l -> l
+            // .logoutUrl("/logout").permitAll()
+            .logoutSuccessUrl("/")
+            .permitAll()
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+        );
+        // .csrf(c -> c
+        //     .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        // );
+        return http.build();
+    }
+}
